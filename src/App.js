89d5d7494d96070da5a9c00e43fbce119770c6f4
DhruvIOS <shahdhruv684@gmail.com>
2023-06-03 21:57:53 -0400

    fixedAnimations

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ function App() {
             <br />
             to <span className="UNFOR">Unforgettable</span> in 3 simple steps
           </h1>
-          <p ref={fade}>
+          <p class="hidden" ref={fade}>
             At Solar Marketing, we help businesses succeed in the digital world{" "}
             <br /> with our proven 3-step process. Our team of experts works
             closely with you <br />
@@ -128,9 +128,10 @@ function App() {
             goals.{" "}
           </p>
 
-          <a
+          <a class="hidden"
+            ref={fade}
             href="https://www.honeybook.com/widget/solar_marketing_255986/cf_id/646e152889bb93002ae42172"
-            target="_blank" ref={fade}
+            target="_blank" 
           >
             {" "}
             Speak to our team
